8b5e14dde2aafe31a5b823b001846762e170097f
lmo <21tyrantlmo@gmail.com>
2024-05-11 18:02:39 +0630

    change dic

--- src/TheWholepage.jsx/const.jsx
+++ src/TheWholepage.jsx/const.jsx
@@ -2,7 +2,7 @@ const About = [
   {
     description: `
     I'm Kylie, a passionate photographer and graphic designer from Bangkok, Thailand. As the founder of CHALAP STUDIO, I specialize in creating stunning graphic designs and capturing captivating photographs.`,
-    img: "./public/profile.png",
+    img: "/dist/profile.png",
   },
 ];
 
